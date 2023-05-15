94ff7ecbc1f42c7dbc25378a18159e3b4e203d3e
parolesparoles <133641187+parolesparoles@users.noreply.github.com>
2023-05-15 15:44:47 +0700

    Update caesar-cipher.py

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.write("hello world!")
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print("Welcome to Python's Online Caesar Cipher Device!")
+key = int(input("Please insert your key here: "))
+newMessage = ' '
+message = input("Please enter a character to message: ")
+for character in message:
+    if character in alphabet:
+        position = alphabet.find(character)
+        newPosition = (position + key) % 26
+        newCharacter = alphabet[newPosition]
+        newMessage += newCharacter
+    else:
+        newMessage += character
+print("Your new message is :", newMessage)
