974b214258b75ea068ee306d52afcec4eeac0066
parolesparoles <133641187+parolesparoles@users.noreply.github.com>
2023-05-15 15:59:57 +0700

    Update caesar-cipher.py

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.text_input("Ur mom?")
+st.title("Online Caesar Cipher Decipher Device")
+st.text("Welcome to Python's Online Caesar Cipher Decipher Device!")
+st.selectbox("Do you wish to encrypt or decrypt your messages?", ['Encrypt', 'Decrypt'])
