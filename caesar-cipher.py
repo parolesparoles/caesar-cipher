822808bf936f2ea3d3987713d73403e143c78b9b
parolesparoles <133641187+parolesparoles@users.noreply.github.com>
2023-06-05 15:16:22 +0700

    Update caesar-cipher.py

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -6,7 +6,7 @@ st.text("Do you wish to encrypt or decrypt your messages?")
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 newMessage = ' '
 
-if st.button("Encryption"): 
+if st.button("Encryption"):
   key = st.number_input("Please insert your key as an integer here: ", 0, 26)
   message = st.text_input("Please enter a message to encrypt: ")
   for character in message:
